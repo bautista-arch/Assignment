9c57bbde31ec1763d0e41841829392cb4280b1da
bautista-arch <bautistakearl@gmail.com>
2026-02-01 09:19:06 +0800

    Couple

---
+++ Couple.java
@@ -0,0 +1,17 @@
+public class Couple {
+    private Person bride;
+    private Person groom;
+
+    public Couple(Person bride, Person groom) {
+        this.bride = bride;
+        this.groom = groom;
+    }
+
+    public Person getBride() {
+        return bride;
+    }
+
+    public Person getGroom() {
+        return groom;
+    }
+}
